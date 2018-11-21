be63fef16aa28234c125cac033486d19b3fae49f
Henning Kasch <henning.kasch@fh-kiel.de>
2018-11-21 11:16:42 +0100

    Hide login and register if user is already logged in

--- src/index.php
+++ src/index.php
@@ -32,12 +32,14 @@ User::startSession();
                 <li class="nav-item active">
                     <a class="nav-link" href="<?= Routing::getUrlToSite('home'); ?>">Home <span class="sr-only">(current)</span></a>
                 </li>
+                <?php if(!User::isLoggedIn()): ?>
                 <li class="nav-item">
                     <a class="nav-link" href="<?= Routing::getUrlToSite('login'); ?>">Log in</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="<?= Routing::getUrlToSite('register'); ?>">Register</a>
                 </li>
+                <?php endif; ?>
             </ul>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
