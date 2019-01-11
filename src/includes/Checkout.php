fa1eddb94742dd54828d46f19f8bcaba45c19921
Erik Hofer <erik-hofer@mail.de>
2019-01-11 16:08:41 +0000

    use transaction when placing an order

--- src/includes/Checkout.php
+++ src/includes/Checkout.php
@@ -24,57 +24,67 @@ class Checkout
     }
 
     static function placeOrder($payment_method = null) {
-        $data = self::getData();
-        $user_id = User::getUserData()['id'];
-        $items = Item::getBasketItemsForCurrentSession();
+        try {
+            $db = DatabaseConnection::getInstance();
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $db->beginTransaction();
 
-        foreach($items as $item) {
-            $stock = Item::getStock($item['id']);
-            if($stock < $item['quantity']) {
-                return "Insufficient stock for article: " . $item['name'];
+            $data = self::getData();
+            $user_id = User::getUserData()['id'];
+            $items = Item::getBasketItemsForCurrentSession();
+
+            foreach($items as $item) {
+                $stock = Item::getStock($item['id']);
+                if($stock < $item['quantity']) {
+                    $db->rollBack();
+                    return "Insufficient stock for article: " . $item['name'];
+                }
             }
-        }
 
-        $db = DatabaseConnection::getInstance();
-        if($payment_method == null && isset($data['creditCardInstitute'])) {
-            $institute = $data['creditCardInstitute'];
-            $number = $data['cardnumber'];
-            $payment = $institute . " *" . substr($number, -3);
-        } else {
-            $payment = $payment_method;
-            // additional payment methods could be handled here
-        }
+            if($payment_method == null && isset($data['creditCardInstitute'])) {
+                $institute = $data['creditCardInstitute'];
+                $number = $data['cardnumber'];
+                $payment = $institute . " *" . substr($number, -3);
+            } else {
+                $payment = $payment_method;
+                // additional payment methods could be handled here
+            }
 
-        $stmt = $db->prepare('INSERT INTO orders (user_id, address, payment_method, date) VALUES (:user_id, :address, :payment, :date)');
-        $success = $stmt->execute([
-            ':user_id' => $user_id,
-            ':address' => $data['address'],
-            ':payment' => $payment,
-            ':date' => date('y-m-d H:i:s')
-        ]);
-        if(!$success) {
-            return "Order could not be placed";
-        }
-        $order_id = $db->lastInsertId();
 
-        foreach($items as $item) {
-            $stmt = $db->prepare('INSERT INTO order_positions VALUES (:product_id, :quantity, :price, :order_id)');
+            $stmt = $db->prepare('INSERT INTO orders (user_id, address, payment_method, date) VALUES (:user_id, :address, :payment, :date)');
             $success = $stmt->execute([
-                ':product_id' => $item['id'],
-                ':quantity' => $item['quantity'],
-                ':price' => Item::getPrice($item['id']),
-                ':order_id' => $order_id
+                ':user_id' => $user_id,
+                ':address' => $data['address'],
+                ':payment' => $payment,
+                ':date' => date('y-m-d H:i:s')
             ]);
-            Item::reduceStock($item['id'], $item['quantity']);
-            Item::removeFromBasket($item['id']);
+            if(!$success) {
+                $db->rollBack();
+                return "Order could not be placed";
+            }
+            $order_id = $db->lastInsertId();
 
-            $stmt = $db->prepare('DELETE FROM basket_positions WHERE user_id = :user_id AND product_id = :product_id');
-            $success = $stmt->execute([
-                ':user_id' => $_SESSION['user_id'],
-                ':product_id' => $item['id']
-            ]);
+            foreach($items as $item) {
+                $stmt = $db->prepare('INSERT INTO order_positions VALUES (:product_id, :quantity, :price, :order_id)');
+                $success = $stmt->execute([
+                    ':product_id' => $item['id'],
+                    ':quantity' => $item['quantity'],
+                    ':price' => Item::getPrice($item['id']),
+                    ':order_id' => $order_id
+                ]);
+                Item::reduceStock($item['id'], $item['quantity']);
+                Item::removeFromBasket($item['id']);
+
+                $stmt = $db->prepare('DELETE FROM basket_positions WHERE user_id = :user_id AND product_id = :product_id');
+                $success = $stmt->execute([
+                    ':user_id' => $_SESSION['user_id'],
+                    ':product_id' => $item['id']
+                ]);
+            }
+            $db->commit();
+        } catch(Exception $e) {
+            $db->rollBack();
         }
-        self::reset();
         return null;
     }
 }
